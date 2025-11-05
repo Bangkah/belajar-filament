cd1fbe17b24c022c36ae14b481551bc320cfa558
Muhammad Dhiyaul Atha <mdhyaulatha@gmail.com>
2025-11-06 03:30:04 +0700

    first commit

---
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+
+class CategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->label('Name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('description')
+                    ->label('Description')
+                    ->required(),
+            ]);
+    }
+}
