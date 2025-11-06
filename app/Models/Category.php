83b2706b802ebd43f5fdd2f9233c45296d40a29e
Muhammad Dhiyaul Atha <mdhyaulatha@gmail.com>
2025-11-06 13:34:29 +0700

    add: Post

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
-{
+ {
     protected $fillable = ['name', 'description'];
-}
+    
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+ }
+
